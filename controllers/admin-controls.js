c17fa56f6bbe4a0a26d6a1026620e8c6337a6209
Mohit Jain <mohitjain9875@gmail.com>
2024-03-23 14:02:35 +0530

    Initial server files, and broad layout

---
+++ controllers/admin-controls.js
@@ -0,0 +1,19 @@
+exports.createDiscountCode = (req, res, next) => {
+  // Generate discount code
+}
+
+exports.getItemsPurchasedList = (req, res, next) => {
+  // Get list of items purchased
+}
+
+exports.getTotalPurchaseAmount = (req, res, next) => {
+  // Get total purchase amount
+}
+
+exports.getDiscountCodesList = (req, res, next) => {
+  // Get discount codes list
+}
+
+exports.getTotalDiscountAmount = (req, res, next) => {
+  // Get total discount amount
+}
---
+++ controllers/auth.js
@@ -0,0 +1,15 @@
+exports.signUpUser = (req, res, next) => {
+  // Sign up user
+}
+
+exports.logInUser = (req, res, next) => {
+  // Manual sign-in validation
+}
+
+exports.validateUserAuthToken = (req, res, next) => {
+  // Extract user id here from JWT
+}
+
+exports.isUserAdmin = (req, res, next) => {
+  // Get user id here and check if that user is admin
+}
