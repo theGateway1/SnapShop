ded6dbc9288955ac108fd5bca02b69603a10e6ce
Mohit Jain <mohitjain9875@gmail.com>
2024-03-23 15:25:44 +0530

    Successful mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const AdminRouter = require('./routes/admin-only')
 
-// Specify routes
+// App routes
 app.use('/admin', AdminRouter)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server listening on port ${port}`))
+
+// Connect to database
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.UNIBLOX_MONGODB)
+    console.log('Connected to database')
+  } catch (error) {
+    console.error('Failed to connect to database', error)
+  }
+}
+
+connectToDatabase()
