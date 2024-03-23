ffa9851f8eca9a2d9bc8266d4e1afa2970f23647
Mohit Jain <mohitjain9875@gmail.com>
2024-03-23 23:13:12 +0530

    Create order API

---
+++ server/routes/order.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const AuthController = require('../controllers/auth')
+const OrderController = require('../controllers/order')
+
+// Create new order
+router.post(
+  '/create-order',
+  AuthController.validateUserAuthToken,
+  OrderController.createNewOrder,
+)
