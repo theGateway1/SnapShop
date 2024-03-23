9ece526b19ac98c6ffb825fd907e0d33a13ac10d
Mohit Jain <mohitjain9875@gmail.com>
2024-03-23 17:55:43 +0530

    Update database name

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ app.listen(port, () => console.log(`Server listening on port ${port}`))
 // Connect to database
 async function connectToDatabase() {
   try {
-    await mongoose.connect(process.env.UNIBLOX_MONGODB)
+    await mongoose.connect(process.env.UNIBLOX_MONGODB, {
+      dbName: 'uniblox',
+    })
+
     console.log('Connected to database')
   } catch (error) {
     console.error('Failed to connect to database', error)
