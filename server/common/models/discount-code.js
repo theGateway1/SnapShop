e191f9eac514533942b570a0b0494f26e4e07e9e
Mohit Jain <mohitjain9875@gmail.com>
2024-03-23 19:15:16 +0530

    Design decisions: Discount code schema

---
+++ server/common/models/discount-code.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+// Schema for a discount code
+
+/**
+ * Design decisions made for this schema:
+ * 1. Storing discount amount here will make it easier to retreive total discountAmount, by just going through each document and adding the this field
+ * 2. Instead of creating a unique code everytime in the server, and verifying whether it is unique or not, let mongoDB's unique document ID generation handle that for us.
+ * 3. Discount code will be generated for a particular order, the orderId in the schema will point to that particular order
+ */
+
+const discountCodeSchema = mongoose.Schema({
+  discountAmount: { type: Number, required: true },
+  orderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Order',
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('DiscountCode', discountCodeSchema)
