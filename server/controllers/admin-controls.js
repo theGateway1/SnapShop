ad06c9061597d11b1a2721c325be07a530837f90
Mohit Jain <mohitjain9875@gmail.com>
2024-03-23 23:14:00 +0530

    Admin method: Generate discount code

--- server/controllers/admin-controls.js
+++ server/controllers/admin-controls.js
@@ -1,7 +1,64 @@
 'use strict'
+const HttpStatus = require('http-status-codes')
+const DiscountCode = require('../common/models/discount-code')
 
+// Controller for admin-only methods
+
+/**
+ * Helper function to create new discount code
+ * @param {Request} orderId - The orderId for which this code will be used
+ * @param {Request} discountPercent - The discount in percentage being applied
+ * @param {Request} discountAmount - The amount being discounted
+ * @returns Discount Code
+ */
+exports.createDiscountCodeHelper = (
+  orderId,
+  discountPercent,
+  discountAmount,
+) => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (!orderId || !discountAmount || !discountPercent) {
+        throw new Error('Missing request parameters')
+      }
+
+      // Generate discount code
+      const discountCode = new DiscountCode({})
+    } catch (error) {}
+  })
+}
+
+/**
+ * Route middleware to create new discount code using the helper
+ * @param {Request} req.body.orderId - The orderId for which this code will be used
+ * @param {Request} req.body.discountPercent - The discount in percentage being applied
+ * @param {Request} req.body.discountAmount - The amount being discounted
+ * @returns Discount Code
+ */
 exports.createDiscountCode = (req, res, next) => {
-  // Generate discount code
+  try {
+    const orderId = req.body.orderId
+    const discountPercent = req.body.discountPercent
+    const discountAmount = req.body.discountAmount
+
+    if (!orderId || !discountAmount || !discountPercent) {
+      throw new Error('Missing request parameters')
+    }
+
+    // Generate discount code using helper
+    this.createDiscountCodeHelper(orderId, discountPercent, discountAmount)
+      .then((discountCode) => {
+        req.discountCode = discountCode
+        next()
+      })
+      .catch((error) => {
+        console.error(error)
+        res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ error })
+      })
+  } catch (error) {
+    console.error(error)
+    res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ error })
+  }
 }
 
 exports.getItemsPurchasedList = (req, res, next) => {
