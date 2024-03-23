29e15f23aa978758eac012715426adc2a6ad595a
Mohit Jain <mohitjain9875@gmail.com>
2024-03-23 23:14:16 +0530

    Add typedefs for order status

--- server/common/typedefs.js
+++ server/common/typedefs.js
@@ -2,4 +2,13 @@ exports.Results = {
   SUCCESS: 'Success',
   INTERNAL_SERVER_ERROR: 'Internal Server Error',
   EXCEPTION: 'Exception',
+  UNAUTHORIZED: 'Unauthorized',
+}
+
+exports.OrderStatus = {
+  CREATED: 'created',
+  SHIPPED: 'shipped',
+  CANCELLED: 'cancelled',
+  DELIVERED: 'delivered',
+  COMPLETED: 'completed',
 }
