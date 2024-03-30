cb67c41a027b545c9b16ed867a066b5c93846ffc
Mohit Jain <mohitjain9875@gmail.com>
2024-03-30 18:16:26 +0530

    Bring products and cart in-sync

--- frontend/src/shared/contexts/cart-context.js
+++ frontend/src/shared/contexts/cart-context.js
@@ -10,19 +10,32 @@ export const CartProvider = ({ children }) => {
   // Helper functions to update items in cart
   const addToCart = (product) => {
     setCart([...cart, { ...product, quantity: 1 }]);
+    const updatedProducts = products.map((item) =>
+      item._id === product._id ? { ...item, quantity: 1 } : item,
+    );
+    setProducts(updatedProducts);
   };
 
   const removeFromCart = (productId) => {
     const updatedCart = cart.filter((item) => item._id !== productId);
+    const updatedProducts = products.map((item) =>
+      item._id === productId ? { ...item, quantity: 0 } : item,
+    );
+
     setCart(updatedCart);
+    setProducts(updatedProducts);
   };
 
   const changeQuantity = (product, newQuantity) => {
     const updatedCart = cart.map((item) =>
       item._id === product._id ? { ...item, quantity: newQuantity } : item,
     );
+    const updatedProducts = products.map((item) =>
+      item._id === product._id ? { ...item, quantity: newQuantity } : item,
+    );
 
     setCart(updatedCart);
+    setProducts(updatedProducts);
   };
 
   const contextValue = {
